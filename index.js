e64da092cd7ed61b2b12d318fca74f14f8de03a0
RuthRojas <ruro122020@gmail.com>
2023-07-01 12:53:57 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(array, eventName){
+  //create new array with the messages 
+  const newArray=[]
+  for(let i = 0; i < array.length; i++){
+    //create custom message for each name
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+  }
+  //return new array
+  return newArray
+}
+
+function countDown(integer){
+  let i = 0;
+  while(i <= integer){
+    console.log(integer)
+    integer--
+  }
+}
